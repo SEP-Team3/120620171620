b3f0aa2661e2261042e4e277fca221db02b24509
Nguyễn Trung Tín <tint139402@gmail.com>
2017-05-24 12:00:47 +0700

    1200

--- Prototype_SEP_Team3/Program.cs
+++ Prototype_SEP_Team3/Program.cs
@@ -17,7 +17,7 @@ namespace Prototype_SEP_Team3
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GUI_Login());
         }
     }
 }
