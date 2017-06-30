c1959df702ddebc56f967c044a4123b96e23e1fc
sangluu884 <sangluu884@gmail.com>
2017-06-30 16:28:58 +0700

    30/6/2017
    Sang commit source code len master

--- Prototype_SEP_Team3/GUI_Login.Designer.cs
+++ Prototype_SEP_Team3/GUI_Login.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -46,13 +46,13 @@
             this.label3.TabIndex = 11;
             this.label3.Text = "MẬT KHẨU";
             // 
-            // textBox2
+            // txtMatKhau
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(209, 173);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(375, 24);
-            this.textBox2.TabIndex = 10;
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhau.Location = new System.Drawing.Point(209, 173);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(375, 24);
+            this.txtMatKhau.TabIndex = 10;
             // 
             // label2
             // 
@@ -64,23 +64,24 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "EMAIL";
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(461, 223);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 38);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "ĐĂNG NHẬP";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(461, 223);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(123, 38);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "ĐĂNG NHẬP";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
-            // textBox1
+            // txtEmail
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(209, 124);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(375, 24);
-            this.textBox1.TabIndex = 7;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(209, 124);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(375, 24);
+            this.txtEmail.TabIndex = 7;
             // 
             // label1
             // 
@@ -96,14 +97,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(714, 308);
+            this.ClientSize = new System.Drawing.Size(730, 306);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtMatKhau);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.label1);
             this.Name = "GUI_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GUI_Login";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -113,10 +115,10 @@
         #endregion
 
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtMatKhau;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Label label1;
     }
 }
--- Prototype_SEP_Team3/GUI_Login.cs
+++ Prototype_SEP_Team3/GUI_Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prototype_SEP_Team3.Admin;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,74 @@ namespace Prototype_SEP_Team3
         public GUI_Login()
         {
             InitializeComponent();
+        }
+
+        public TaiKhoan Find(string email)
+        {
+            DBEntities db = new DBEntities();
+            TaiKhoan acc = new TaiKhoan();
+            return db.TaiKhoans.FirstOrDefault(x => x.Email == email);
+        }
+
+        public int Test()
+        {
+            TaiKhoan kq = null;
+
+            var nv = Find(txtEmail.Text);
+
+            if ((nv != null) && (nv.MatKhau.Equals(txtMatKhau.Text)))
+            {
+                kq = nv;
+            }
+
+            int re = kq.Id;
+            return re;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            DBEntities model = new DBEntities();
+
+            var nv = Find(txtEmail.Text);
+
+            if (nv == null)
+            {
+                MessageBox.Show("Incorrect email");
+            }
+            else
+            {
+                if (!nv.MatKhau.Equals(txtMatKhau.Text))
+                {
+                    MessageBox.Show("Incorrect Password");
+                }
+            }
+
+            if ((nv != null) && (nv.MatKhau.Equals(txtMatKhau.Text)))
+            {
+                PhanQuyenTaiKhoan pq = model.PhanQuyenTaiKhoans.FirstOrDefault(x => x.TaiKhoan_Id == nv.Id);
+
+                if (pq.ChucVu == "Admin")
+                {
+                    this.Hide();
+                    GUI_Admin main = new GUI_Admin();
+                    main.Closed += (s, args) => this.Close();
+                    main.ShowDialog();
+                }
+                else if (pq.ChucVu == "Giáo vụ")
+                {
+                    this.Hide();
+                    GUI_Chinh_GV main = new GUI_Chinh_GV(Test());
+                    main.Closed += (s, args) => this.Close();
+                    main.ShowDialog();
+                }
+                else
+                {
+                    this.Hide();
+                    GUI_Chinh_BST_GVGD main = new GUI_Chinh_BST_GVGD(Test());
+                    main.Closed += (s, args) => this.Close();
+                    main.ShowDialog();
+                }
+            }
         }
     }
 }
